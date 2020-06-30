bbfe14261087dd80be42396442e883c93df39288
arihant-2310 <arihant2310@gmail.com>
2020-06-30 17:40:33 +0530

    fixes

--- src/components/Navbars/PublicNavBar.jsx
+++ src/components/Navbars/PublicNavBar.jsx
@@ -3,13 +3,13 @@ import { A } from "hookrouter";
 export default function PublicNavBar() {
     return (
         <nav className="flex items-center justify-between flex-wrap bg-indigo-500">
-            <a href="/">
+            <A href="/">
                 <div className="flex items-center flex-shrink-0 text-white mr-6 py-6 pl-6">
                     <span className="font-semibold text-xl ml-1 tracking-tight">
                         Chatbot- CoronaAdmin
                     </span>
                 </div>
-            </a>
+            </A>
         </nav>
     );
 }
